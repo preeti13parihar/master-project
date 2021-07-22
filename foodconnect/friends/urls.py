5fdd545920b04313e828966243c0ab811e71023b
preeti13parihar <preeti1998parihar@gmail.com>
2021-07-21 21:51:25 -0700

    Added friend request apis

---
+++ foodconnect/friends/urls.py
@@ -0,0 +1,39 @@
+from django.conf.urls import url
+from .views import FriendsList, FriendViewSet
+
+
+add_friend = FriendViewSet.as_view({
+    'post': 'add_friend'
+})
+
+accept_request = FriendViewSet.as_view({
+    'get': 'accept_request'
+})
+
+reject_request = FriendViewSet.as_view({
+    'get': 'reject_request'
+})
+
+cancel_request = FriendViewSet.as_view({
+    'get': 'cancel_request'
+})
+
+list_requests = FriendViewSet.as_view({
+    'get': 'list_requests'
+})
+
+
+list_friends = FriendViewSet.as_view({
+    'get': 'list_friends'
+})
+
+
+urlpatterns = [
+    url(r'^list/(?P<user_id>\w+)', list_friends, name='list_friends'),
+    url(r'^add', add_friend, name='add_friend'),
+    url(r'^accept/(?P<friendship_request_id>\d+)', accept_request, name='accept_request'),
+    url(r'^reject/(?P<friendship_request_id>\d+)', reject_request, name='reject_request'),
+    url(r'^cancel/(?P<friendship_request_id>\d+)', cancel_request, name='cancel_request'),
+    url(r'requests', list_requests, name='list_requests'),
+
+]
