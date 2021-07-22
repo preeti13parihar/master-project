ceaeaa0263f96cb81a7bbc4ae6829a7f43608ff4
preeti13parihar <preeti1998parihar@gmail.com>
2021-07-21 22:17:43 -0700

    Added User location apis

--- foodconnect/authentication/profile.py
+++ foodconnect/authentication/profile.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import action
 from rest_framework import permissions
 
 from authentication.models import User
-from authentication.serializers import UserSerializer, UpdateUserSerializer
+from authentication.serializers import UserSerializer, UpdateUserSerializer, LocationSerializer
 
 
 class ProfileUpdateAPI(generics.RetrieveUpdateAPIView):
@@ -27,8 +27,17 @@ class ProfileUpdateAPI(generics.RetrieveUpdateAPIView):
         return get_object_or_404(User, uuid=uid)
 
 
-# class ProfileDeleteAPI(generics.DestroyAPIView):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-#     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-#     lookup_url_kwarg = "uid"
+class LocationUpdateAPI(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = LocationSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    lookup_url_kwarg = "uid"
+
+
+    def put(self, request, *args, **kwargs):
+        kwargs["partial"] = True
+        return super().update(request, *args, **kwargs)
+
+    def get_object(self,):
+        uid = self.kwargs["uid"]
+        return get_object_or_404(User, uuid=uid)
