52fd383de19d2c7ac56758bda7be7272edb0170d
prideven <70807326+prideven@users.noreply.github.com>
2021-11-24 00:36:51 +0530

    Update settings.py

--- foodconnect/foodconnect/settings.py
+++ foodconnect/foodconnect/settings.py
@@ -4,12 +4,9 @@ import json
 from urllib import request
 from pathlib import Path
 from datetime import timedelta
-import pymysql
 
-pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -50,7 +47,6 @@ SESSION_COOKIE_DOMAIN = os.getenv("SESSION_COOKIE_DOMAIN", "localhost")
 SESSION_COOKIE_SECURE = os.getenv("SESSION_COOKIE_SECURE", True)
 SESSION_COOKIE_EXPIRY = os.getenv("SESSION_COOKIE_EXPIRY", 1)
 
-
 if not APP_CLIENT_ID:
     print("Please set COGNITO_APP_CLIENT_ID !!!")
 elif not APP_SECRET_KEY:
@@ -79,120 +75,131 @@ HTTP_ONLY_COOKIE = True
 USE_CSRF = False
 AUTO_CREATE_USER = True
 SECURE_COOKIE = True
-APPEND_SLASH=True
+APPEND_SLASH=False
 
 CORS_WHITELIST_FILEPATH = os.getenv("CORS_WHITELIST_FILEPATH", "./cors.list")
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+'django.contrib.admin',
+'django.contrib.auth',
+'django.contrib.contenttypes',
+'django.contrib.sessions',
+'django.contrib.messages',
+'django.contrib.staticfiles',
 
-    'core',
-    'trail',
-    'reviews',
-    'foodzone',
-    'friends',
-    'friendship',
-    'authentication',
-    'rest_framework',
-    'corsheaders'
+'core',
+'trail',
+'reviews',
+'foodzone',
+'friends',
+'friendship',
+'authentication',
+'rest_framework',
+'corsheaders'
 ]
-
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+'corsheaders.middleware.CorsMiddleware',
+'django.middleware.security.SecurityMiddleware',
+'django.contrib.sessions.middleware.SessionMiddleware',
+'django.middleware.common.CommonMiddleware',
+# 'django.middleware.csrf.CsrfViewMiddleware',
+'django.contrib.auth.middleware.AuthenticationMiddleware',
+'django.contrib.messages.middleware.MessageMiddleware',
+'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
-    # 'django.contrib.auth.middleware.RemoteUserMiddleware',
-    'authentication.middleware.cognito_django_middleware.AwsDjangoMiddleware',
+# 'django.contrib.auth.middleware.AuthenticationMiddleware',
+# 'django.contrib.auth.middleware.RemoteUserMiddleware',
+'authentication.middleware.cognito_django_middleware.AwsDjangoMiddleware',
 ]
-
 AUTHENTICATION_BACKENDS = [
-    # 'django.contrib.auth.backends.RemoteUserBackend',
-    # 'django.contrib.auth.backends.ModelBackend',
-    'authentication.middleware.cognito_rest_authentication.AwsRestAuthentication'
+# 'django.contrib.auth.backends.RemoteUserBackend',
+# 'django.contrib.auth.backends.ModelBackend',
+'authentication.middleware.cognito_rest_authentication.AwsRestAuthentication'
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'core.api.permissions.DenyAny',
-    ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'authentication.middleware.cognito_rest_authentication.AwsRestAuthentication',
+'DEFAULT_PERMISSION_CLASSES': (
+'core.api.permissions.DenyAny',
+),
+'DEFAULT_AUTHENTICATION_CLASSES': (
+'authentication.middleware.cognito_rest_authentication.AwsRestAuthentication',
 
-        # 'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        # 'rest_framework_simplejwt.authentication.JWTAuthentication'
-    ),
+# 'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+# 'rest_framework_simplejwt.authentication.JWTAuthentication'
+),
 }
 
 ROOT_URLCONF = 'foodconnect.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+{
+'BACKEND': 'django.template.backends.django.DjangoTemplates',
+'DIRS': [],
+'APP_DIRS': True,
+'OPTIONS': {
+'context_processors': [
+'django.template.context_processors.debug',
+'django.template.context_processors.request',
+'django.contrib.auth.context_processors.auth',
+'django.contrib.messages.context_processors.messages',
+],
+},
+},
+]
+
+CORS_ALLOW_HEADERS = [
+'accept',
+'accept-encoding',
+'authorization',
+'content-type',
+'dnt',
+'origin',
+'user-agent',
+'x-csrftoken',
+'x-requested-with',
+'accesstoken',
+'refreshtoken',
+'access-control-allow-origin',
+'access-control-allow-headers'
 ]
 
 WSGI_APPLICATION = 'foodconnect.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': DB_NAME,
-        'USER': DB_USERNAME,
-        'PASSWORD': DB_PASSWORD,
-        'HOST': HOST,   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
-    }
+'default': {
+'ENGINE': 'django.db.backends.mysql',
+'NAME': DB_NAME,
+'USER': DB_USERNAME,
+'PASSWORD': DB_PASSWORD,
+'HOST': HOST, # Or an IP Address that your DB is hosted on
+'PORT': '3306',
+'OPTIONS': {
+'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
 }
-
+}
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+{
+'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+},
+{
+'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+},
+{
+'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+},
+{
+'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+},
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
@@ -206,7 +213,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
