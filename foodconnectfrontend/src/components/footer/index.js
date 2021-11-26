e646ed531f00d36081e5ae629991b1fc516a5242
priyanka <prideven09@gmail.com>
2021-11-25 17:29:27 -0800

    change header

--- foodconnectfrontend/src/components/footer/index.js
+++ foodconnectfrontend/src/components/footer/index.js
@@ -9,7 +9,7 @@ export default function Footer() {
     <>
       <footer>
         <div className="container">
-          <p><Link to='/'>Breadcrumbs</Link> &copy; 2016</p>
+          <p><Link to='/'>FoodConnect</Link> &copy; 2021</p>
         </div>
       </footer>
     </>
--- foodconnectfrontend/src/containers/Home/index.js
+++ foodconnectfrontend/src/containers/Home/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Footer from '../../components/footer/index';
 import Header from "../../components/header/index";
-import Image from '../../images/banner.jpg';
+import Image from '../../images/foodConnect2.jpeg';
 import './home.css';
 
 export default function Home() {
@@ -12,7 +12,7 @@ export default function Home() {
       <main className="home">
         <section className="banner" style={{ backgroundImage: 'url(' + Image + ')' }}>
           <div className="container">
-            <h1>Breadcrumbs</h1>
+            <h1>FoodConnect</h1>
             <p>The ultimate social media network for foodies.
             Remember where you've been and what you've eaten!
           </p>
