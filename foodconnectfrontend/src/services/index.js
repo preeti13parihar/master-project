2b3986cc5cc27128ae6699fb7371919ed744e561
preeti13parihar <preeti1998parihar@gmail.com>
2021-11-29 00:57:53 -0800

    fix: filename

--- foodconnectfrontend/src/services/index.js
+++ foodconnectfrontend/src/services/index.js
@@ -5,7 +5,7 @@ import axios from 'axios';
  */
 
 const customAxios = axios.create({
-  baseURL: 'http://localhost:8000/app',
+  baseURL: 'http://foodconnect.nktoolbox.com/app',
 });
 
 customAxios.interceptors.request.use(
